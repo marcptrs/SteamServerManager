0d45a030d018857cce968137b888e410c5da9a0e
Marc Peters <mapeters@outlook.com>
2023-02-27 21:20:02 -0600

    wip: initial project architecture with packages added

---
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SteamServerManager.Infrastructure.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+}
---
+++ src/WebUI/Server/Controllers/ApiControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MediatR;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using SteamServerManager.WebUI.Server.Filters;
+
+namespace SteamServerManager.WebUI.Server.Controllers;
+
+[ApiController]
+[ApiExceptionFilter]
+[Route("api/[controller]")]
+public abstract class ApiControllerBase : ControllerBase
+{
+    private ISender _mediator = null!;
+    protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+}
+
